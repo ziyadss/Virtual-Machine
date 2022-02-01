ba684c09792dbf8ce8c70f258eea65aa1834ccba
Ziyad <ziyad.ss@hotmail.com>
2022-02-01 19:30:29 +0200

    Implemented most traps
    tputsp remaining

--- src/traps.c
+++ src/traps.c
@@ -1,18 +1,50 @@
 #pragma once
 
 #include "header.h"
+#include "memory.c"
 
 #define T_COUNT 8
 #define TRAP_OFFSET 0x20
 typedef void (*trap_t)();
 
-static inline void tgetc(){};
-static inline void tout(){};
-static inline void tputs(){};
-static inline void tin(){};
-static inline void tputsp(){};
-static inline void thalt(){};
-static inline void tinu16(){};
-static inline void toutu16(){};
+static inline void tgetc()
+{
+    registers[R0] = getchar();
+};
+
+static inline void tout()
+{
+    putchar(registers[R0]);
+};
+
+static inline void tputs()
+{
+    word_t *p = memory + registers[R0];
+    while (*p)
+        putchar(*p++);
+};
+
+static inline void tin()
+{
+    // registers[R0] = getchar();
+    // putchar(registers[R0]);
+    putchar(registers[R0] = getchar());
+};
+
+static inline void tputsp(){/* TODO: */};
+
+static inline void thalt()
+{
+    running = false;
+};
+
+static inline void tinu16()
+{
+    scanf("%hu", &registers[R0]);
+};
+static inline void toutu16()
+{
+    printf("%hu\n", registers[R0]);
+};
 
 trap_t traps[8] = {tgetc, tout, tputs, tin, tputsp, thalt, tinu16, toutu16};
