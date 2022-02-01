5ee2821f691997a7d86e7f472d6b003c5cd645b7
Ziyad <ziyad.ss@hotmail.com>
2022-02-01 19:30:09 +0200

    Renamed memory variable

--- src/memory.c
+++ src/memory.c
@@ -2,7 +2,7 @@
 
 #include "header.h"
 
-word_t mem[MEMORY_SIZE] = {0};
+word_t memory[MEMORY_SIZE] = {0};
 
-static inline word_t mem_read(word_t address) { return mem[address]; }
-static inline void mem_write(word_t address, word_t val) { mem[address] = val; }
+static inline word_t mem_read(word_t address) { return memory[address]; }
+static inline void mem_write(word_t address, word_t val) { memory[address] = val; }
