79a341d32d361eface82cdd9f53fff147405633d
Ziyad Sameh <ziyad.ss@hotmail.com>
2022-03-26 10:36:56 +0200

    Made incorrect run command print to error stream

--- src/main.c
+++ src/main.c
@@ -33,7 +33,8 @@ int main(int argc, char **argv)
 {
     if (argc != 2)
     {
-        printf("Usage: %s <file>\n", argv[0]);
+        
+        fprintf(stderr, "Usage: %s <file>\n", argv[0]);
         exit(1);
     }
 
